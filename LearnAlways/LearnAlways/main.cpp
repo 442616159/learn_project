f7010d0ffce37954614f42f0496e8195398ea8b8
不太懂是吧 <442616159@qq.com>
2025-06-09 22:31:55 +0800

    Add a new project

---
+++ LearnAlways/LearnAlways/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LearnAlways
+//
+//  Created by 不太懂是吧 on 2025/6/9.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
